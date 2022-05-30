15d648f54f846d26acb00ff5e3012d37bd654084
rui-exe <mrswager173@gmail.com>
2022-05-30 10:48:19 +0100

    Minor changes

--- Cenario.cpp
+++ Cenario.cpp
@@ -15,10 +15,10 @@ void Cenario::Cenario_1_1() {
     cout<<"Path: Max Flow"<<endl;
     auto start = chrono::steady_clock::now();
     list<int> path = dataset.dijkstra_path(1, dataset.n);
+    auto end = chrono::steady_clock::now();
     for(auto iter=path.begin();iter!=path.end();iter++){
         cout << "Node " << *iter<< endl;
     }
-    auto end = chrono::steady_clock::now();
     cout << "Tempo de execucao: "<<chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms"<< endl;
 }
 
@@ -35,9 +35,9 @@ void Cenario::Cenario_1_2() {
     cout<<"Path: Menos Transbordos"<<endl;
     auto start = chrono::steady_clock::now();
     list<int> stops_changes = dataset.unweighted_path(1,dataset.n);
+    auto end = chrono::steady_clock::now();
     for(auto iter=stops_changes.begin();iter!=stops_changes.end();iter++)
         cout << "Node " << *iter<< endl;
-    auto end = chrono::steady_clock::now();
     cout << "Tempo de execucao: "<<chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms"<< endl;
 
 }
@@ -47,9 +47,8 @@ void Cenario::Cenario_2_1() {
     int dimension;
     cout<<"Qual a dimensao do grupo desejada: "; cin>>dimension;
     cout << endl<< endl<<"2.1"<<endl <<endl;
+    auto start = chrono::steady_clock::now();
     dataset.addEdge(0,1,dimension,0);
-
-
     int max_flow = dataset.edmonds_karp(0,dataset.n);
     cout << max_flow << endl;
     pair<list<int>,int> group;
@@ -58,6 +57,8 @@ void Cenario::Cenario_2_1() {
     group.first=path;
     group.second=max_flow;
     dataset.print_path(group);
+    auto end = chrono::steady_clock::now();
+    cout << "Tempo de execucao: "<<chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms"<< endl;
 }
 
 void Cenario::Cenario_2_2() {
@@ -67,6 +68,7 @@ void Cenario::Cenario_2_2() {
 void Cenario::Cenario_2_3() {
     dataset = db.getDataset();
     cout<<endl<<endl<<"2.3"<<endl <<endl;
+    auto start = chrono::steady_clock::now();
     int max_flow = dataset.edmonds_karp(1,dataset.n);
     cout << max_flow << endl;
     pair<list<int>,int> group;
@@ -75,6 +77,9 @@ void Cenario::Cenario_2_3() {
     group.first=path;
     group.second=max_flow;
     dataset.print_path(group);
+    auto end = chrono::steady_clock::now();
+    cout <<endl<<"Max Flow = " <<max_flow << endl;
+    cout << "Tempo de execucao: "<<chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms"<< endl;
 }
 
 void Cenario::Cenario_2_4() {
@@ -83,7 +88,7 @@ void Cenario::Cenario_2_4() {
     cout<<endl<<endl<<"2.4"<<endl <<endl;
     auto start = chrono::steady_clock::now();
     int durMin = dataset.criticalPath();
-    cout << durMin << endl;
+    cout <<"Tempo total de demora: "<< durMin <<" horas." <<endl;
     auto end = chrono::steady_clock::now();
     cout << "Tempo de execucao: "<<chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms"<< endl;
 }
@@ -97,11 +102,11 @@ void Cenario::Cenario_2_5() {
     dataset.getLF(durMin);
     dataset.putInfoOnEdges();
     pair<int,list<int>> waiting_nodes = dataset.getMaxWaitingTime();
+    auto end = chrono::steady_clock::now();
     cout << "People waited " << waiting_nodes.first << " hours in the following nodes" << endl;
     for(auto iter=waiting_nodes.second.begin();iter!=waiting_nodes.second.end();iter++){
         cout << "Node " << *iter << endl;
 
     }
-    auto end = chrono::steady_clock::now();
     cout << "Tempo de execucao: "<<chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms"<< endl;
 }
